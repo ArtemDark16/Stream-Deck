bba0100249f8f894c3f22d9007d57d361767269f
ArtemDark16 <81185612+ArtemDark16@users.noreply.github.com>
2026-06-03 11:02:39 +0300

    Delete kursova directory

--- kursova/UiControls.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "framework.h"
-
-HWND CreateLabel(HWND parent, const wchar_t* text, int x, int y, int w, int h);
-HWND CreateLabelWithId(HWND parent, int id, const wchar_t* text, int x, int y, int w, int h);
-HWND CreateEdit(HWND parent, int id, int x, int y, int w, int h);
-HWND CreateCombo(HWND parent, int id, int x, int y, int w, int h);
