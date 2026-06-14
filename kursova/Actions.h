7c06e6ca99a323c0e1530868586a2d3935629f28
ArtemDark16 <81185612+ArtemDark16@users.noreply.github.com>
2026-06-14 17:13:33 +0300

    Delete kursova directory

--- kursova/Actions.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "framework.h"
-
-#include <string>
-
-std::wstring ExpandEnvironment(const std::wstring& text);
-void SwitchToPage(int index);
-void ExecuteButton(int index);
